b48dde543801d4ee9f501f458c32d06d6c75145f
syafiqmk <syafiqmuh.k@gmail.com>
2023-08-21 05:38:41 +0800

    initial commit"

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+    // echo "Hello World!!";
+
+    // Variable
+    // String
+    $name = "Budi";
+    $kelamin = 'Laki-laki';
+    // Integer
+    $age = 25;
+    // Float
+    $berat = 90.5;
+    $jarak = 7.3;
+    // Boolean
+    $lulus = false;
+
+    // var_dump($name);
+
+    echo "Nama : $name <br> Umur : $age <br> Kelamin : $kelamin <br> Berat badan : $berat Kg";
+    echo "<hr>";
+    echo "Jarak rumah $name ke BPVP Samarinda adalah $jarak KM";
+    var_dump($lulus);
+?>
